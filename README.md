0c314f6c027e3cce02b742e0f7ac17efdd175fba
Krunal Patil <krunal.patil@smartthings.com>
2017-10-05 16:33:02 -0700

    Add script to deploy dotfiles.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dotfiles
-My dotfiles
+```
+Usage: ./deploy.sh <install|remove> <path for dotfiles repo>
+```
---
+++ deploy.sh
@@ -0,0 +1,68 @@
+
+install()
+{
+    ln -s $DOT_FILES_REPO/home/.Xresources ~/.Xresources
+    ln -s $DOT_FILES_REPO/home/.bash-completion/ ~/.bash-completion
+    ln -s $DOT_FILES_REPO/home/.bash_logout ~/.bash_logout
+    ln -s $DOT_FILES_REPO/home/.bash_profile ~/.bash_profile
+    ln -s $DOT_FILES_REPO/home/.bashrc ~/.bashrc
+    ln -s $DOT_FILES_REPO/home/.config/ ~/.config
+    ln -s $DOT_FILES_REPO/home/.dir_colors ~/.dir_colors
+    ln -s $DOT_FILES_REPO/home/.fluxbox/ ~/.fluxbox 
+    ln -s $DOT_FILES_REPO/home/.gitconfig ~/.gitconfig 
+    ln -s $DOT_FILES_REPO/home/.gitignore_global ~/.gitignore_global
+    ln -s $DOT_FILES_REPO/home/.vimrc ~/.vimrc
+    ln -s $DOT_FILES_REPO/home/.inputrc ~/.inputrc 
+    ln -s $DOT_FILES_REPO/home/.scripts/ ~/.scripts 
+    ln -s $DOT_FILES_REPO/home/.tmux.conf ~/.tmux.conf
+    ln -s $DOT_FILES_REPO/home/.vim/ ~/.vim
+    ln -s $DOT_FILES_REPO/home/.vim_undo/ ~/.vim_undo
+    ln -s $DOT_FILES_REPO/home/.xinitrc ~/.xinitrc 
+    ln -s $DOT_FILES_REPO/home/.zsh/ ~/.zsh
+    ln -s $DOT_FILES_REPO/home/.zshrc ~/.zshrc
+}
+
+remove()
+{
+    rm -rf ~/.Xresources
+    rm -rf ~/.bash-completion
+    rm -rf ~/.bash_logout
+    rm -rf ~/.bash_profile
+    rm -rf ~/.bashrc
+    rm -rf ~/.bashrc.swp
+    rm -rf ~/.config
+    rm -rf ~/.dir_colors
+    rm -rf ~/.fluxbox
+    rm -rf ~/.gitconfig
+    rm -rf ~/.gitignore_global
+    rm -rf ~/.vimrc
+    rm -rf ~/.inputrc
+    rm -rf ~/.scripts
+    rm -rf ~/.tmux.conf
+    rm -rf ~/.vim
+    rm -rf ~/.vim_undo
+    rm -rf ~/.xinitrc
+    rm -rf ~/.zsh
+    rm -rf ~/.zshrc
+}
+
+usage()
+{
+    echo "Usage: ./deploy.sh <install|remove> <path for dotfiles repo>"
+    exit
+}
+
+# Entry point for script
+if [ $# -lt 2 ]; then
+    usage
+fi
+
+CMD="$1"
+DOT_FILES_REPO="$2"
+
+case $CMD in
+    "install") install;;
+    "remove") remove;;
+    (*)
+        usage;;
+esac
